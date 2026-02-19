7966f6e831266caa40d14915cacd08debca63a68
Douye Oseke <2830113O@student.gla.ac.uk>
2026-02-19 18:40:52 +0000

    commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
