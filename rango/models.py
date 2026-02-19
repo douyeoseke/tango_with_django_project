a6739d6a3dff8c5e94f810806b614d1a3c6368d0
Douye Oseke <2830113O@student.gla.ac.uk>
2026-02-19 18:35:19 +0000

    adding page class

--- rango/models.py
+++ rango/models.py
@@ -10,3 +10,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
